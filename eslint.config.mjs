77f4240f22d3b7b8dc93a7027da461ebb46a3bf0
Erwin Martinez <erwinmart17m@gmail.com>
2026-02-28 03:08:25 -0600

    Pequeños cambios

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,22 @@
 import js from '@eslint/js'
-import globals, { jest, node } from 'globals'
+import globals from 'globals'
 
 export default [
   {
     ignores: ['eslint.config.mjs', 'node_modules']
   },
+
   js.configs.recommended,
+
   {
     languageOptions: {
-      globals: globals.node,
       ecmaVersion: 'latest',
-      sourceType: 'commonjs'
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
     },
-    rules: {},
-    env: {
-      jest: true,
-      node: true
-    }
+    rules: {}
   }
-]
+];
